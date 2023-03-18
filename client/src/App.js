87ffa12c31d4159c0eb217b0904be9ad69dd9c0c
elnurxo <elnurykh@code.edu.az>
2023-03-18 22:23:00 +0400

    initial commit, client config

---
+++ client/src/App.js
@@ -0,0 +1,8 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Login from './components/Login';
+
+function App() {
+  return <Login/>
+}
+
+export default App;
---
+++ client/src/components/Login.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Container } from 'react-bootstrap';
+const redirect_uri = 'http://localhost:3000';
+const AUTH_URL =
+  `https://accounts.spotify.com/authorize?client_id=b96a5a503973474cb7c99f854a33cb8c&response_type=code&redirect_uri=${redirect_uri}&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state`;
+
+function Login() {
+  return <Container className="d-flex justify-content-center align-items-center" style={{minHeight:'100vh'}}>
+    <a className="btn btn-success btn-lg" href={AUTH_URL}>Login With Spotify</a>
+  </Container>;
+}
+
+export default Login;
