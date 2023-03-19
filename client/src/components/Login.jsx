26c6a1e12638af1d7390462166adeb86a4facd8e
elnurxo <elnurykh@code.edu.az>
2023-03-19 14:35:29 +0400

    production bug fix

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container } from 'react-bootstrap';
-const redirect_uri = 'http://localhost:3000';
+const redirect_uri = 'https://spotifybyelnurxo.netlify.app/';
 const AUTH_URL =
   `https://accounts.spotify.com/authorize?client_id=b96a5a503973474cb7c99f854a33cb8c&response_type=code&redirect_uri=${redirect_uri}&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state`;
 
